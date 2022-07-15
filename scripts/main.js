98acd4343a33f1bcbcc0a08ef9d5b5b310befbe7
Santiago Valdez <santiagodval@outlook.com>
2022-07-14 21:12:10 -0300

    styles good, and js corrected so each id is dif

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@
 let section = {
     title: "",
     type: "",
-    subsections: []
+    subsections: [],
+    i: 0
 }
 
 localStorage.getItem("sections") === null ? localStorage.setItem("sections",JSON.stringify({})) : generateHTML()
@@ -58,8 +59,10 @@ function addTextfield() {
 
 function addSubsectionToElement() {
     console.log(document.getElementById("subsectionText").value);
-    section.subsections.push(document.getElementById("subsectionText").value);
-
+    section.subsections.push({value:document.getElementById("subsectionText").value, number:section.i});
+    section.i = section.i + 1;
+    section.subsections[0].i;
+    console.log(section.i);
 }
 
 //-------------
@@ -70,7 +73,7 @@ function addHTML() {
 
         let container = document.createElement("fieldset");
 
-        container.className = "container";
+        container.className = "fieldset";
         container.innerHTML = `<legend>${section.title}</legend> ${createHTML()}`;
         
         document.body.append(container);
@@ -85,7 +88,8 @@ function resetSection() {
     section = {
         title: "",
         type: "",
-        subsections: []
+        subsections: [],
+        i:section.i
     }
 }
 
@@ -94,7 +98,7 @@ function resetSection() {
 function createHTML(){
     let html ="";
     array = section.subsections;
-    array.forEach((coso,index) => {html = html+(`<label for='cbox1' class='container'>${coso}<input type='${section.type}' id='cbox${index}' value='${coso}'><span class='checkmark'></span></label><br>`)});
+    array.forEach((coso,index) => {html = html+(`<label for='cbox${coso.number}' class='container'>${coso.value}<input type='${section.type}' id='cbox${coso.number}' value='${coso.value}'><span class='checkmark'></span></label><br>`)});
     console.log(html)
     return html;
 }
@@ -102,8 +106,6 @@ function createHTML(){
 
 //creates and add the html to the dom from local storage
 function generateHTML(){
-    console.log(JSON.parse(localStorage.getItem("sections")));
-    console.log(Object.keys(JSON.parse(localStorage.getItem("sections"))).length);
     for(let i = 0;i<Object.keys(JSON.parse(localStorage.getItem("sections"))).length;i++){
         
         section.type = JSON.parse(localStorage.getItem("sections"))[i].type;
@@ -111,6 +113,7 @@ function generateHTML(){
         section.subsections = JSON.parse(localStorage.getItem("sections"))[i].subsections;
         addHTML();
         resetSection();
+        section.i = JSON.parse(localStorage.getItem("sections"))[i].i;
     }
     
 }
